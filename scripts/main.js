56c6364ddbe42c37d4dbd185305bed0223844fe5
AlexMarshe <alkhateeb.alex@hotmail.com>
2023-01-10 16:14:54 +0300

    added css grid-areas, better css for buttons, updated html file with needed grid classes

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,6 @@ import { updateThemeMode, updateThemeScheme } from "./Theme.js";
 //assign click event listenrs to buttons with their appropriate function invocations
 document.querySelectorAll("[data-color]").forEach(b => b.addEventListener("click", (e) => updateThemeScheme(e.target.value)));
 document.querySelector("[data-theme-mode = toggle]").addEventListener("click", updateThemeMode)
+// const svg = document.querySelector("svg  path.cls-1");
+// console.log(svg)
+// // svg.setAttribute('style', "fill:#182094")
